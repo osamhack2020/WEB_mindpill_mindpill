0a5574bed9f3efa66771f79361db040085cf47eb
Yongbin Kim <iam@yongbin.kim>
2020-10-09 10:52:43 +0000

    backend/api: Response 관련 Helper 추가

--- backend/handlers/api/error.go
+++ backend/handlers/api/error.go
@@ -3,7 +3,6 @@ package api
 import (
 	"errors"
 	"mindpill/backend/internal/debug"
-	"net/http"
 	"strconv"
 
 	"github.com/valyala/fasthttp"
@@ -12,14 +11,15 @@ import (
 type ErrorResp struct {
 	status int
 	err    error
+	msg    string
 }
 
 func Error(status int, err error) *ErrorResp {
-	return &ErrorResp{status, err}
+	return &ErrorResp{status, err, ""}
 }
 
 func ErrorString(status int, err string) *ErrorResp {
-	return &ErrorResp{status, errors.New(err)}
+	return &ErrorResp{status, errors.New(err), ""}
 }
 
 func (e *ErrorResp) Error() string {
@@ -32,11 +32,15 @@ func (e *ErrorResp) Unwrap() error {
 
 func (e *ErrorResp) Write(ctx *fasthttp.RequestCtx) {
 	ctx.SetStatusCode(e.status)
-	if e.status == http.StatusInternalServerError && !debug.IsDebug() {
-		ctx.WriteString(`{"message":"An error occurred on the server."}`)
-	} else {
-		ctx.WriteString(`{"message":`)
+	if e.err != nil && debug.IsDebug() {
+		ctx.WriteString(`{"error":`)
 		ctx.WriteString(strconv.Quote(e.err.Error()))
-		ctx.WriteString(`}`)
+		ctx.WriteString(`,`)
+	} else {
+		ctx.WriteString(`{`)
 	}
+	ctx.WriteString(`"message":`)
+	ctx.WriteString(strconv.Quote(e.msg))
+	ctx.WriteString(`}`)
+	ctx.Write([]byte{'\n'})
 }
--- backend/handlers/api/helpers.go
+++ backend/handlers/api/helpers.go
@@ -2,16 +2,81 @@ package api
 
 import (
 	"bytes"
+	"encoding/json"
 	"errors"
+	"mindpill/backend/internal/database"
+	"mindpill/backend/internal/tokens"
+	"mindpill/ent/user"
+
+	"mindpill/backend/internal/log"
 
 	"github.com/valyala/fasthttp"
+	"go.uber.org/zap"
 )
 
-func ParseAuthorization(ctx fasthttp.RequestCtx) ([]byte, error) {
+var logger = log.Logger()
+
+// Permission
+
+func IsAdmin(ctx *fasthttp.RequestCtx, userID int) bool {
+	r, err := database.Ent().
+		User.Query().
+		Where(user.IDEQ(userID)).
+		QueryAdmin().
+		Exist(ctx)
+	if err != nil {
+		logger.Error(
+			"api: admin query failed",
+			zap.Error(err),
+		)
+		return false
+	}
+	return r
+}
+
+// Token
+
+func ParseAuthorization(ctx *fasthttp.RequestCtx) (*tokens.Token, error) {
 	header := ctx.Request.Header.Peek("authorization")
 	headerParts := bytes.SplitN(header, []byte{' '}, 2)
-	if !bytes.Equal(headerParts[0], []byte("bearer")) {
+	if !bytes.Equal(bytes.ToUpper(headerParts[0]), []byte("BEARER")) {
 		return nil, errors.New("authorization header must be bearer token")
 	}
-	return headerParts[1], nil
+	return tokens.Validate(headerParts[1])
+}
+
+// Responses
+
+func SendResponse(ctx *fasthttp.RequestCtx, data interface{}) {
+	buf, err := json.Marshal(data)
+	if err != nil {
+		InternalServerError(ctx, err, "failed to marshaling data")
+		return
+	}
+	ctx.Write(buf)
+}
+
+func BadRequest(ctx *fasthttp.RequestCtx, err error, msg string) {
+	resp := &ErrorResp{fasthttp.StatusBadRequest, err, msg}
+	resp.Write(ctx)
+}
+
+func Unauthorized(ctx *fasthttp.RequestCtx, err error, msg string) {
+	resp := &ErrorResp{fasthttp.StatusUnauthorized, err, msg}
+	resp.Write(ctx)
+}
+
+func NotFound(ctx *fasthttp.RequestCtx, err error, msg string) {
+	resp := &ErrorResp{fasthttp.StatusNotFound, err, msg}
+	resp.Write(ctx)
+}
+
+func MethodNotAllowed(ctx *fasthttp.RequestCtx, err error, msg string) {
+	resp := &ErrorResp{fasthttp.StatusMethodNotAllowed, err, msg}
+	resp.Write(ctx)
+}
+
+func InternalServerError(ctx *fasthttp.RequestCtx, err error, msg string) {
+	resp := &ErrorResp{fasthttp.StatusInternalServerError, err, msg}
+	resp.Write(ctx)
 }
