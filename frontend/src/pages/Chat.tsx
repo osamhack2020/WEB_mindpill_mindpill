1f3b30b97cac6fd16203fba37afcfb0c4408592d
현우 김 <clo3olb@gmail.com>
2020-10-11 12:46:19 +0000

    ChatBrowser 생성 및 상담페이지 접속시 채팅방 목록으로 접속할 수 있도록 설정.

--- frontend/src/pages/Chat.tsx
+++ frontend/src/pages/Chat.tsx
@@ -1,53 +1,14 @@
 import React from 'react'
-import { NavLink, Route, Switch } from 'react-router-dom'
+import { NavLink, Redirect, Route, Switch } from 'react-router-dom'
 
 export interface ChatRoomProps {
   selected?: boolean
 }
 export interface ChatRoomStates {}
 
-export class ChatRoom extends React.Component<ChatRoomProps, ChatRoomStates> {
-  render() {
-    return (
-      <>
-        <div className="profile-image">
-          <i className="fas fa-user"></i>
-        </div>
-        <div className="wrapper">
-          <div className="name">홍길동 상담관</div>
-          <span className="last-message">무슨 고민이 있나요?</span>
-          <span className="last-message-time">10분전</span>
-        </div>
-      </>
-    )
-  }
-}
-
-export interface ChatLogProps {
-  text: string
-  timestamp: string
-  myLog: boolean
-}
-
-export class ChatLog extends React.Component<ChatLogProps> {
-  render() {
-    return (
-      <div className={`chat-log ${this.props.myLog ? 'myLog' : 'opponentLog'}`}>
-        <div className="wrapper">
-          <div className="text">{this.props.text}</div>
-          <div className="timestamp">{this.props.timestamp}</div>
-        </div>
-        {!this.props.myLog && (
-          <div className="profile-image">
-            <i className="fas fa-user"></i>
-          </div>
-        )}
-      </div>
-    )
-  }
-}
-
 export class UserInfo extends React.Component {
+  // 이곳에 있는 모든 정보를 Props에서 가져올 수 있도록 바꿔야 합니다.
+
   render() {
     return (
       <div className="user-info box-top-column expand">
@@ -90,10 +51,36 @@ export class UserInfo extends React.Component {
   }
 }
 
+export interface ChatLogProps {
+  text: string
+  timestamp: string
+  myLog: boolean
+}
+
+export class ChatLog extends React.Component<ChatLogProps> {
+  render() {
+    return (
+      <div className={`chat-log ${this.props.myLog ? 'myLog' : 'opponentLog'}`}>
+        <div className="wrapper">
+          <div className="text">{this.props.text}</div>
+          <div className="timestamp">{this.props.timestamp}</div>
+        </div>
+        {!this.props.myLog && (
+          <div className="profile-image">
+            <i className="fas fa-user"></i>
+          </div>
+        )}
+      </div>
+    )
+  }
+}
+
 export class CurrentChatRoom extends React.Component {
   componentDidMount = () => {
     this.scrollToBottom()
   }
+  // 채팅내역을 로드할 수 있는 API가 필요합니다. (채팅방 id)
+
   state = {
     chatLogs: [
       {
@@ -108,33 +95,9 @@ export class CurrentChatRoom extends React.Component {
       },
       {
         text:
-          '이것은 테스트 텍스트입니다세요. 이것은 테스트 텍스트입니다이것은 테스트 텍스트입니다세요. 이것은 테스트 텍스트입니다이것은 테스트 텍스트입니다세요. 이것은 테스트 텍스트입니다',
-        timestamp: '10:10 am',
-        myLog: true
-      },
-      {
-        text:
           '안녕하세요. 이것은 상대방 테스트 텍스트입니다안녕하세요. 이것은 상대방 테스트 텍스트입니다안녕하세요. 이것은 상대방 테스트 텍스트입니다',
         timestamp: '10:12 am',
         myLog: false
-      },
-      {
-        text:
-          '이것은 테스트 텍스트입니다세요. 이것은 테스트 텍스트입니다이것은 테스트 텍스트입니다세요. 이것은 테스트 텍스트입니다이것은 테스트 텍스트입니다세요. 이것은 테스트 텍스트입니다',
-        timestamp: '10:10 am',
-        myLog: true
-      },
-      {
-        text:
-          '안녕하세요. 이것은 상대방 테스트 텍스트입니다안녕하세요. 이것은 상대방 테스트 텍스트입니다안녕하세요. 이것은 상대방 테스트 텍스트입니다',
-        timestamp: '10:12 am',
-        myLog: false
-      },
-      {
-        text:
-          '이것은 테스트 텍스트입니다세요. 이것은 테스트 텍스트입니다이것은 테스트 텍스트입니다세요. 이것은 테스트 텍스트입니다이것은 테스트 텍스트입니다세요. 이것은 테스트 텍스트입니다',
-        timestamp: '10:10 am',
-        myLog: true
       },
       {
         text:
@@ -164,7 +127,7 @@ export class CurrentChatRoom extends React.Component {
     this.setState(
       {
         chatLogs: [
-          ...this.state.chatLogs,
+          ...this.state.chatLogs, //기존 채팅로그
           {
             text: message,
             timestamp: this.getTimestamp(),
@@ -212,7 +175,30 @@ export class CurrentChatRoom extends React.Component {
   }
 }
 
+//채팅방 목록에 들어가는 채팅방 셀 한개
+export class ChatRoom extends React.Component<ChatRoomProps, ChatRoomStates> {
+  //이곳에 있는 모든 정보를 Props에서 가져올 수 있도록 변경해야 합니다.
+  render() {
+    return (
+      <>
+        <div className="profile-image">
+          <i className="fas fa-user"></i>
+        </div>
+        <div className="wrapper">
+          <div className="name">홍길동 상담관</div>
+          <span className="last-message">무슨 고민이 있나요?</span>
+          <span className="last-message-time">10분전</span>
+        </div>
+      </>
+    )
+  }
+}
+
+export interface ChatRoomListProps {}
+
 export class ChatRoomList extends React.Component {
+  //현재 사용자가 열람할 수 있는 모든 채팅방을 가져올 수 있는 API가 필요합니다.
+
   render() {
     return (
       <div className="chatroom-list">
@@ -236,42 +222,57 @@ export class CounselorList extends React.Component {
   }
 }
 
-export default class Chat extends React.Component {
+export interface ChatBrowserProps {}
+export class ChatBrowser extends React.Component {
+  render() {
+    return (
+      <>
+        <div className="chat-navbar">
+          <div className="profile-image-area">
+            <div className="profile-image">
+              <i className="fas fa-user"></i>
+            </div>
+          </div>
+          <ul>
+            <NavLink to="/chat/chatrooms" activeClassName="selected">
+              <li>
+                <i className="fas fa-comment-alt"></i>
+              </li>
+            </NavLink>
+            <NavLink to="/chat/counselors" activeClassName="selected">
+              <li>
+                <i className="fas fa-users"></i>
+              </li>
+            </NavLink>
+          </ul>
+        </div>
+        <div className="chat-navbar-content">
+          <div className="chat-search">
+            <div className="chat-search-input">
+              <i className="fas fa-search"></i>
+              <input type="text" placeholder="검색"></input>
+            </div>
+          </div>
+          <Switch>
+            <Redirect exact path="/chat" to="/chat/chatrooms" /> {/** /chat/chatrooms 를 기본 페이지로 설정하기 위함. */}
+            <Route path="/chat/chatrooms" component={ChatRoomList} />
+            <Route path="/chat/counselors" component={CounselorList} />
+          </Switch>
+        </div>
+      </>
+    )
+  }
+}
+
+export interface ChatProps {}
+export interface ChatStates {}
+
+export default class Chat extends React.Component<ChatRoomProps, ChatRoomStates> {
   render() {
     return (
       <div className="box-left expand">
         <div className="chat">
-          <div className="chat-navbar">
-            <div className="profile-image-area">
-              <div className="profile-image">
-                <i className="fas fa-user"></i>
-              </div>
-            </div>
-            <ul>
-              <NavLink to="/chat/chatrooms" activeClassName="selected">
-                <li>
-                  <i className="fas fa-comment-alt"></i>
-                </li>
-              </NavLink>
-              <NavLink to="/chat/counselors" activeClassName="selected">
-                <li>
-                  <i className="fas fa-users"></i>
-                </li>
-              </NavLink>
-            </ul>
-          </div>
-          <div className="chat-navbar-content">
-            <div className="chat-search">
-              <div className="chat-search-input">
-                <i className="fas fa-search"></i>
-                <input type="text" placeholder="검색"></input>
-              </div>
-            </div>
-            <Switch>
-              <Route path="/chat/chatrooms" component={ChatRoomList} />
-              <Route path="/chat/counselors" component={CounselorList} />
-            </Switch>
-          </div>
+          <ChatBrowser />
 
           <Route path="/chat/chatrooms/:id">
             <div className="chat-content">
