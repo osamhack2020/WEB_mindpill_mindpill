e76ca594c675fbb49146e0c0c18c49af77fbac9b
현우 김 <clo3olb@gmail.com>
2020-10-13 11:14:19 +0000

    NotFound Page 함수 컴포넌트로 변환

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,7 +1,5 @@
 import React from 'react'
 
-export default class PageNotFound extends React.Component {
-  render() {
-    return <div>404 Not Found</div>
-  }
+export default function PageNotFound() {
+  return <div>404 Not Found</div>
 }
