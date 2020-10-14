8559d21298a07f98e22ac2fc78d812848dc64c0c
현우 김 <clo3olb@gmail.com>
2020-10-14 09:37:36 +0000

    Component 함수 컴포넌트로 변환

--- frontend/src/components/SelectBox.tsx
+++ frontend/src/components/SelectBox.tsx
@@ -1,76 +1,70 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-export interface OptionProps {
+type OptionProps = {
   value: string
-  onChangeValue?: (value: string) => void
+  changeValue?: (value: string) => void
 }
 
-export class Option extends React.Component<OptionProps> {
-  handleClick = () => {
-    if (this.props.onChangeValue != null) {
-      this.props.onChangeValue(this.props.value)
+export function Option({ value, changeValue }: OptionProps) {
+  function handleClick() {
+    if (changeValue != null) {
+      changeValue(value)
     }
   }
 
-  render() {
-    return (
-      <div className="option" onClick={this.handleClick}>
-        {this.props.value}
-      </div>
-    )
-  }
+  return (
+    <div className="option" onClick={handleClick}>
+      {value}
+    </div>
+  )
 }
 
-export interface SelectBoxProps {
+type SelectBoxProps = {
   name: string
   values: Array<string>
   placeholder?: string
   required?: boolean
 }
 
-export interface SelectBoxState {
+type SelectBoxState = {
   chosenValue: string
 }
 
-export default class SelectBox extends React.Component<SelectBoxProps, SelectBoxState> {
-  state = {
-    chosenValue: ''
-  }
+export default function SelectBox({ name, values, placeholder, required }: SelectBoxProps) {
+  const [chosenValue, setChosenValue] = useState<string>('')
 
-  changeValue = (value: string) => {
-    this.setState({ chosenValue: value })
+  function changeValue(value: string) {
+    setChosenValue(value)
   }
-  handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
     e.preventDefault()
   }
-  handleFocus = (e: React.ChangeEvent<HTMLInputElement>) => {
+  function handleFocus(e: React.ChangeEvent<HTMLInputElement>) {
     e.target.blur()
   }
-  render() {
-    return (
-      <div className="input-select">
-        <input
-          value={this.state.chosenValue}
-          placeholder={this.props.placeholder}
-          type="text"
-          name={this.props.name}
-          onChange={this.handleChange}
-          required={this.props.required}
-          onFocus={this.handleFocus}
-          pattern={
-            '/' +
-            this.props.values.reduce((a, c) => {
-              return (a = a + '|' + c)
-            }) +
-            '/'
-          }
-        />
-        <div className="options">
-          {this.props.values.map((value, key) => (
-            <Option value={value} onChangeValue={this.changeValue} key={key} />
-          ))}
-        </div>
+  return (
+    <div className="input-select">
+      <input
+        value={chosenValue}
+        placeholder={placeholder}
+        type="text"
+        name={name}
+        onChange={handleChange}
+        required={required}
+        onFocus={handleFocus}
+        pattern={
+          '/' +
+          values.reduce((a, c) => {
+            return (a = a + '|' + c)
+          }) +
+          '/'
+        }
+      />
+      <div className="options">
+        {values.map((value, key) => (
+          <Option value={value} changeValue={changeValue} key={key} />
+        ))}
       </div>
-    )
-  }
+    </div>
+  )
 }
