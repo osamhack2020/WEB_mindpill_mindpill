dc4338b6f5b94ed04db94b14ecf0051ffb05a791
현우 김 <clo3olb@gmail.com>
2020-10-25 07:06:13 +0000

    회원가입 플로우 수정  및 팝업창 생성

--- frontend/src/pages/Join.tsx
+++ frontend/src/pages/Join.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 
@@ -14,6 +14,9 @@ type JoinQuery = {
 }
 
 export default function Join() {
+  const [popup, setPopup] = useState<boolean>(false)
+  const [joinSuccess, setJoinSuccess] = useState<boolean>(false)
+
   function handleSubmit(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault()
     if (e.currentTarget.password.value == e.currentTarget.password2.value) {
@@ -42,56 +45,91 @@ export default function Join() {
       data: query
     })
       .then(response => {
-        const { status, data } = response
+        const { status, statusText, data } = response
+        if (status == 200 && statusText == 'OK') {
+          setJoinSuccess(true)
+          setPopup(true)
+        }
         console.log({ response })
         // accessToken을 localStorage, cookie 등에 저장하지 않는다!
       })
       .catch(error => {
+        setPopup(true)
         console.log(error)
       })
   }
 
+  function handleDismiss() {
+    setPopup(false)
+  }
+
   return (
     <div id="page_join">
-      <div className="greetings">
-        마인드필에 오신 것을 <br />
-        환영합니다!
+      <div id="page_join_content">
+        <div className="greetings">
+          마인드필에 오신 것을 <br />
+          환영합니다!
+        </div>
+        <form id="form_join" onSubmit={handleSubmit}>
+          <input className="styled sized" name="userName" type="text" placeholder="이름" autoFocus required />
+          <input className="styled sized" name="email" type="email" placeholder="이메일" required />
+
+          <select name="gender" className="styled sized" defaultValue="">
+            <option value="" disabled>
+              성별을 선택해주세요.
+            </option>
+            <option value="m">남성</option>
+            <option value="f">여성</option>
+          </select>
+
+          <input className="styled sized" name="password" type="password" placeholder="비밀번호" required />
+          <input className="styled sized" name="password2" type="password" placeholder="비밀번호 확인" required />
+
+          <select name="rank_id" className="styled sized" defaultValue="">
+            <option value="" disabled>
+              계급/등급을 선택해주세요.
+            </option>
+            <option value="2">이등병</option>
+            <option value="3">일병</option>
+            <option value="4">상병</option>
+            <option value="5">병장</option>
+          </select>
+
+          <input className="styled sized" name="phone_number" type="text" placeholder="전화번호" required />
+          <input className="styled sized" name="sv_number" type="text" placeholder="군번" required />
+          {/**소속 검색해서 팝업 띄워야함 */}
+          <input className="styled sized" name="group_id" type="text" placeholder="소속을 검색하세요" required />
+
+          <input className="styled sized black" type="submit" value="회원가입" />
+        </form>
+        <Link to="/login" className="login">
+          <p>이미 회원이신가요?</p>
+        </Link>
       </div>
-      <form id="form_join" onSubmit={handleSubmit}>
-        <input className="styled sized" name="userName" type="text" placeholder="이름" autoFocus required />
-        <input className="styled sized" name="email" type="email" placeholder="이메일" required />
 
-        <select name="gender" className="styled sized" defaultValue="">
-          <option value="" disabled>
-            성별을 선택해주세요.
-          </option>
-          <option value="m">남성</option>
-          <option value="f">여성</option>
-        </select>
-
-        <input className="styled sized" name="password" type="password" placeholder="비밀번호" required />
-        <input className="styled sized" name="password2" type="password" placeholder="비밀번호 확인" required />
-
-        <select name="rank_id" className="styled sized" defaultValue="">
-          <option value="" disabled>
-            계급/등급을 선택해주세요.
-          </option>
-          <option value="2">이등병</option>
-          <option value="3">일병</option>
-          <option value="4">상병</option>
-          <option value="5">병장</option>
-        </select>
-
-        <input className="styled sized" name="phone_number" type="text" placeholder="전화번호" required />
-        <input className="styled sized" name="sv_number" type="text" placeholder="군번" required />
-        {/**소속 검색해서 팝업 띄워야함 */}
-        <input className="styled sized" name="group_id" type="text" placeholder="소속을 검색하세요" required />
-
-        <input className="styled sized black" type="submit" value="회원가입" />
-      </form>
-      <Link to="/login" className="login">
-        <p>이미 회원이신가요?</p>
-      </Link>
+      <div className={`popup_container ${popup && 'on'}`}>
+        <div className="popup">
+          {joinSuccess ? (
+            <>
+              <i className="fas fa-check-circle icon success"></i>
+              <div className="message">회원가입이 완료되었습니다.</div>
+              <Link to="/login">
+                <button className="styled sized black">
+                  <i className="fas fa-sign-in-alt"></i> &nbsp; 로그인
+                </button>
+              </Link>
+            </>
+          ) : (
+            <>
+              <i className="fas fa-exclamation-triangle icon error"></i>
+              <div className="message">정보를 수정해주세요.</div>
+              <button className="styled sized black" onClick={handleDismiss}>
+                <i className="fas fa-undo-alt"></i> &nbsp; 수정하기
+              </button>
+            </>
+          )}
+        </div>
+      </div>
     </div>
   )
 }
